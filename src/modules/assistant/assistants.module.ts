1c91e2d99d06f671d4ed2ea21acacce28532e39d
Alejandro Parra <alejandro.parraparra@externos-cl.cencosud.com>
2024-05-12 00:24:45 -0400

    feat: add initial test for openAI

--- src/modules/assistant/assistants.module.ts
+++ src/modules/assistant/assistants.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AssistantsController } from './assistants.controller';
 import { AssistantsService } from './assistants.service';
 import { OrtographyAssistantController } from './orthography.controller';
+import OpenAI from 'openai';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AssistantsController, OrtographyAssistantController],
-  providers: [AssistantsService],
+  providers: [
+    AssistantsService,
+    {
+      provide: OpenAI,
+      useFactory: (configService: ConfigService) => {
+        const apiKey = configService.get<string>('openai.apiKey');
+        return new OpenAI({
+          apiKey,
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
 export class AssistantsModule {}
--- src/modules/assistant/orthography.controller.ts
+++ src/modules/assistant/orthography.controller.ts
@@ -1,9 +1,28 @@
 import { Controller, Post } from '@nestjs/common';
 
-@Controller('assistant/orthography')
+import OpenAI from 'openai';
+
+@Controller('assistants/orthography')
 export class OrtographyAssistantController {
+  constructor(private readonly openAI: OpenAI) {}
+
   @Post()
   public async orthography() {
-    return 'Hello World!';
+    // TODO: Move to own service and add an abtraction layer to build other assistants
+    const completion = await this.openAI.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content: 'Eres un asistente para corregir ortografia muy util',
+        },
+      ],
+      model: 'gpt-3.5-turbo',
+    });
+
+    if (completion.choices.length >= 1) {
+      return completion.choices.at(0);
+    }
+
+    return 'There is not valid choices. Try it again.';
   }
 }
